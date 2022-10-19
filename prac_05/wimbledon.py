fba0ce0286b267500ece14916107136636b602c3
lador1 <luca.adornetto@my.jcu.edu.au>
2022-10-19 17:53:25 +1000

    create function to get file and convert to dictionary

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,10 +2,35 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
+    get_file()
+    convert_records_to_dictionary()
+
+
+def convert_records_to_dictionary(records):
+    """Create dictionary of champions and set of countries from records (list of lists)."""
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[1])
+        try:
+            champion_to_count[record[1]] += 1
+        except KeyError:
+            champion_to_count[record[2]] = 1
+    return champion_to_count, countries
+
+
+def display_results(records):
+    champion_to_count = {records}
+
+
+def get_file(filename):
+    "Get records from file and display in list"
+    records = []
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         for line in in_file:
-            new = line.split(',')
-            print(new)
+            parts = line.strip().split(',')
+            records.append(parts)
+        return records
 
 
 main()
