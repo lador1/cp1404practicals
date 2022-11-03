d0b9857ef4d10f3e633ce43ea9e5bd014b0a658a
lador1 <luca.adornetto@my.jcu.edu.au>
2022-11-03 20:28:33 +1000

    Completed first task

---
+++ prac_06/used_cars.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical - Client code to use the Car class.
+Note that the import has a folder (module) in it.
+This is why we name our folders with no spaces or capitals, as valid module names.
+"""
+
+from prac_06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car(180, "Car")
+    my_car.drive(30)
+    print(f"Car has fuel: {my_car.fuel}")
+
+    limo = Car(100, "Limo")
+    limo.add_fuel(20)
+    print(f"Limo has fuel: {limo.fuel}")
+    limo.drive(115)
+
+
+main()
