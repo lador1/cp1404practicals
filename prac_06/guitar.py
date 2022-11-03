8bcb4e54ee56c04bcbe5080a30ac4d57af2b8013
lador1 <luca.adornetto@my.jcu.edu.au>
2022-11-03 22:11:04 +1000

    Completed final task

---
+++ prac_06/guitar.py
@@ -0,0 +1,24 @@
+CURRENT_YEAR = 2022
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        guitar_age = CURRENT_YEAR - self.year
+        return guitar_age
+
+    def is_vintage(self):
+        if self.get_age() >= 50:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
