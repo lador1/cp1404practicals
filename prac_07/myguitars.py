4567c702cd485df45e40d8d3d868f1d26663ced1
lador1 <luca.adornetto@my.jcu.edu.au>
2022-11-08 12:45:22 +1000

    add function to update list with new guitar

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -10,7 +10,7 @@ FILENAME = 'guitars.csv'
 
 def main():
     guitars = load_guitars(FILENAME)
-    print_guitars(guitars)
+    print_guitars_list(guitars)
 
 
 def load_guitars(FILENAME):
@@ -23,38 +23,27 @@ def load_guitars(FILENAME):
             guitars.append(guitar)
     return guitars
 
-
-def print_guitars(guitars):
-    print("These are my guitars:")
+def print_guitars_list(guitars):
+    """Print the guitars in the csv file"""
+    print("Guitars loaded from file")
     for i, guitar in enumerate(guitars, 1):
-        print(f"Guitar {i}: {guitar.name:24} ({guitar.year}), worth ${guitar.cost}")
+        print(f"{i}: {guitar.name:24} ({guitar.year}), worth ${guitar.cost}")
+    update_guitars_list(guitars)
 
-# def load_data(filename):
-#
-#
-# def print_guitars(guitars):
-#     print("These are my guitars:")
-#     for i, guitar in enumerate(guitars, 1):
-#         vintage_string = ""
-#         if guitar.is_vintage():
-#             vintage_string = " (vintage)"
-#         print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-#
+
+def update_guitars_list(guitars):
+    """Allow user to enter guitar info and update to list"""
+    name = input("Add a new guitar: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        print(f"{name} ({year}) : ${cost:.2f} added!")
+        new_guitar = Guitar(name, year, cost)  # Needed to make new guitar because append is only for 2 things
+        guitars.append(new_guitar)
+        name = input("Name: ")
+        print("\n... snip ...\n")
+        print_guitars_list(guitars)
+
+
 
 main()
-
-
-
-
-
-# name = input("Name: ")
-#     # while name != "":
-#     #     year = int(input("Year: "))
-#     #     cost = float(input("Cost: "))
-#     #     print(f"{name} ({year}) : ${cost:.2f} added!")
-#     #     new_guitar = Guitar(name, year, cost)  # Needed to make new guitar because append is only for 2 things
-#     #     guitars.append(new_guitar)
-#     #     name = input("Name: ")
-#     #     print("\n... snip ...\n")
-#     #     print_guitars(guitars)
-#
