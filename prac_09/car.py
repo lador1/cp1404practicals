88e62634f28034aeb95003b2423a5c4676cad17f
lador1 <luca.adornetto@my.jcu.edu.au>
2022-11-23 13:58:05 +1000

    Completed walkthrough example

---
+++ prac_09/car.py
@@ -0,0 +1,38 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+
+
+class Car:
+    """Represent a Car object."""
+
+    def __init__(self, name="", fuel=0):
+        """Initialise a Car instance.
+        name: string, reference name for car
+        fuel: float, one unit of fuel drives one kilometre
+        """
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
+
+    def __str__(self):
+        """Return a string representation of a Car object."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self.odometer}"
+
+    def add_fuel(self, amount):
+        """Add amount to the car's fuel."""
+        self.fuel += amount
+
+    def drive(self, distance):
+        """Drive the car a given distance.
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        """
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
---
+++ prac_09/taxi_test.py
@@ -0,0 +1,12 @@
+from prac_09.taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100)
+my_taxi.drive(40)
+
+print(my_taxi)
+
+
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi)
+
