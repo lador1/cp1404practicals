4995fe9e1437507a790f31784e89ffdf0758a999
lador1 <luca.adornetto@my.jcu.edu.au>
2022-11-30 23:27:11 +1000

    Completed testing file

---
+++ prac_10/testing.py
@@ -0,0 +1,59 @@
+"""
+CP1404/CP5632 Practical
+Testing demo using assert and doctest
+"""
+
+import doctest
+from prac_06.car import Car
+
+
+def repeat_string(s, n):
+    """Repeat string s, n times, with spaces in between."""
+    return " ".join([s] * n)
+
+
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
+
+
+def run_tests():
+    """Run the tests on the functions."""
+    assert repeat_string("Python", 1) == "Python"
+    # the test below should fail
+    assert repeat_string("hi", 2) == "hi hi"
+
+    test_car = Car()
+    assert test_car._odometer == 0, "Car does not set odometer correctly"
+
+    test_car = Car(fuel=10)
+    assert test_car.fuel == 10
+    test_car = Car()
+    assert test_car.fuel == 0
+
+
+run_tests()
+
+
+def create_sentence(words):
+    """
+    Create a sentence out of words.
+    >>> create_sentence('my dog is cool')
+    'My dog is cool.'
+    """
+
+    sentence = words.capitalize()
+    if sentence[-1] != ".":
+        sentence += "."
+    return sentence
+
+
+doctest.testmod()
